d657c0982d1bcb836c18d364a973b2fb0932b869
Tomek <19194950+tpintaric@users.noreply.github.com>
2025-02-12 21:49:06 -0700

    Added stats for each character to main.py

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-def word_count():
-    with open("./books/frankenstein.txt") as f:
-        file_contents = f.read()
-        word_count = file_contents.split()
-        print(len(word_count))
-
 def main():
     with open("./books/frankenstein.txt") as f:
         file_contents = f.read()
         print(file_contents)
+    return file_contents
+
+def word_count():
+    word_count = main().split()
+    print(len(word_count))
+
+def character_count(frank):
+    character_stats = {}
+    for w in frank:
+        w_lower = w.lower()
+
+        if w_lower not in character_stats:
+            character_stats[w_lower] = 1
+        else:
+            character_stats[w_lower] += 1
+    print(character_stats)
+
 
 
 main()
 word_count()
+character_count(main())
