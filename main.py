273d78d46962740027d09ee22cf1ece08ce34078
Tomek <19194950+tpintaric@users.noreply.github.com>
2025-06-17 11:24:10 -0600

    Added main and stats file, reading book txts

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-# print("")
+def get_book_text(path):
+    with open(path) as f:
+        book_text = f.read()
+        #print(book_text)
+        return book_text
 
-"""
-with open("./books/frankenstein.txt") as f:
-    book_content = f.read()
-"""
+def get_book_words(entire_book):
+    num_words = len(entire_book.split())
+    print(f"{num_words} words found in the document")
+   
+def main():
+    entire_book = get_book_text("./books/frankenstein.txt")
+    get_book_words(entire_book)
+    
+main()
+    
