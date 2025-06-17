2c8195023c4263d063a7908c113753475a4bf236
Tomek <19194950+tpintaric@users.noreply.github.com>
2025-06-17 11:38:05 -0600

    post refactoring

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
+from stats import get_book_words
+
 def get_book_text(path):
     with open(path) as f:
         book_text = f.read()
         #print(book_text)
         return book_text
-
-def get_book_words(entire_book):
-    num_words = len(entire_book.split())
-    print(f"{num_words} words found in the document")
    
 def main():
     entire_book = get_book_text("./books/frankenstein.txt")
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_book_words(entire_book):
+    num_words = len(entire_book.split())
+    print(f"{num_words} words found in the document")
