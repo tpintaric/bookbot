83d732654dfb9d6f84fa6a1151d76cec855553dd
Tomek <19194950+tpintaric@users.noreply.github.com>
2025-06-17 15:09:51 -0600

    Reversly ordered a list of alpha chars

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from stats import get_book_chars, get_book_words
+from stats import get_book_chars, get_book_words, sorted_book_chars
 
 def get_book_text(path):
     with open(path) as f:
         book_text = f.read()
+        
         #print(book_text)
         return book_text
    
 def main():
     entire_book = get_book_text("./books/frankenstein.txt")
     get_book_words(entire_book)
-    get_book_chars(entire_book)
-    
+    book_char_stats = get_book_chars(entire_book)
+    sorted_book_chars(book_char_stats)
+
 main()
     
--- stats.py
+++ stats.py
@@ -1,13 +1,37 @@
+
 def get_book_words(entire_book):
     num_words = len(entire_book.split())
     print(f"{num_words} words found in the document")
 
 def get_book_chars(entire_book):
-    book_chars = {}
+    book_char_stats = {}
     for c in entire_book:
         lower_c = c.lower()
-        if lower_c not in book_chars:
-            book_chars[lower_c] = 1
+        if lower_c not in book_char_stats:
+            book_char_stats[lower_c] = 1
         else:
-            book_chars[lower_c] +=1
-    print(book_chars)
+            book_char_stats[lower_c] +=1
+    return book_char_stats
+
+def sorted_book_chars(book_char_stats):
+    book_chars_alpha = []
+
+    for ch in book_char_stats:
+        key = ch
+        val = book_char_stats[ch]
+
+        if ch.isalpha() == True:
+            book_chars_alpha.append({"char":key, "num":val} )
+
+    def sort_by(dict):
+        return dict["num"]
+
+    sorted_chars_alpha = sorted(book_chars_alpha, reverse=True, key=sort_by)
+    # Alternative way of writing would be:
+    # book_chars_alpha.sort(reverse=True, key=sort_by)
+    #print(book_chars_alpha)
+
+    print(sorted_chars_alpha)
+    #print(book_chars_alpha) 
+    
+    #return sorted_book_chars
